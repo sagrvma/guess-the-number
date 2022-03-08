e994b057e4319c3db5cdcc9e58da9923ba9c516f
sagrvma <therealsagarverma@gmail.com>
2022-03-08 15:06:05 +0530

    Added comparison of guess and showing score feat.

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Input!';
+  }
+  if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct!';
+    document.querySelector('.number').textContent = secretNumber;
+  } else {
+    if (guess > secretNumber) {
+      document.querySelector('.message').textContent = 'Lower...';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Higher...';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
+  }
+});
